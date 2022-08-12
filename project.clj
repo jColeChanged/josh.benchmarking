a02aae79d2b6a9d3ff5d95a3ec086468f3b969da
Joshua <jcolechanged@gmail.com>
2022-08-11 19:11:01 -0500

    Prepare for first release of josh.benchmarking.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject josh.benchmarking "0.1.0-SNAPSHOT"
+(defproject josh.benchmarking (or (System/getenv "PROJECT_VERSION") "SNAPSHOT-0.1.0")
   :description "A benchmark automation utility."
   :url "https://github.com/jcolechanged/josh.benchmarking"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -11,4 +11,7 @@
                  [techascent/tech.viz "6.00-beta-16-3"]]
   :josh.benchmarking {:benchmark-paths ["benchmarks/"] 
                       :environments [{:namespaces [josh.benchmarking.core]}]}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password}]]
   :eval-in :leiningen)
