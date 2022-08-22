a35b7bbe28dfc11ae04b4a0e61b93342a908804f
Joshua <jColeChanged@gmail.com>
2022-08-21 21:09:39 -0500

    Change project org.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject josh.benchmarking (or (System/getenv "PROJECT_VERSION") "SNAPSHOT-0.1.0")
+(defproject org.clojars.joshua/josh.benchmarking (or (System/getenv "PROJECT_VERSION") "SNAPSHOT-0.1.0")
   :description "A benchmark automation utility."
   :url "https://github.com/jcolechanged/josh.benchmarking"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
